aad71ae0f41f0e3814d759b5b4dec9795743201a
kyoya <kyoryu32@gmail.com>
2022-06-16 04:24:31 +0900

    add: プロジェクト作成

---
+++ UISegmentedControl/UISegmentedControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UISegmentedControl
+//
+//  Created by Yamaguchi Kyoya on 2022/06/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
