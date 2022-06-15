2e8c04e8a6c8ad39c0eb04207fb1d7a56bb3351c
Kyoya Yamaguchi <kyoryu32@gmail.com>
2022-06-16 03:49:15 +0900

    update: 空白行削除, color Literal削除, didSetへの変更

--- UISlider/UISlider/ViewController.swift
+++ UISlider/UISlider/ViewController.swift
@@ -3,27 +3,25 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var numberLabel: UILabel!
-    @IBOutlet var numberSlider: UISlider!
+    @IBOutlet var numberSlider: UISlider! {
+        didSet {
+            //この項目は全てStoryboardで設定できます。書かなくても実行可能です。
+            numberSlider.minimumValue = 0
+            numberSlider.maximumValue = 50
+            numberSlider.minimumTrackTintColor = UIColor.cyan
+            numberSlider.maximumTrackTintColor = UIColor.orange
+            numberSlider.minimumValueImage = UIImage(systemName: "volume")
+            numberSlider.maximumValueImage = UIImage(systemName: "volume.3")
+            numberSlider.setValue(0, animated: false)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupSlider(numberSlider)
-    }
-    
-    func setupSlider(_ slider: UISlider) {
-        //この関数の中の項目は全てStoryboardで設定できます。書かなくても実行可能です。
-        slider.minimumValue = 0
-        slider.maximumValue = 50
-        slider.minimumTrackTintColor = #colorLiteral(red: 0.4156862745, green: 0.768627451, blue: 0.862745098, alpha: 1)
-        slider.maximumTrackTintColor = #colorLiteral(red: 1, green: 0.8392156863, blue: 0.03921568627, alpha: 1)
-        slider.minimumValueImage = UIImage(systemName: "volume")
-        slider.maximumValueImage = UIImage(systemName: "volume.3")
-        slider.setValue(0, animated: false)
     }
     
     @IBAction func didChangeSliderValue(_ slider: UISlider) {
         numberLabel.text = String(slider.value)
     }
-
 }
 
