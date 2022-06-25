0fae8a2413bc6c749ee808afc85f1eb6a1791582
kyoya <kyoryu32@gmail.com>
2022-06-26 05:15:30 +0900

    add: プロジェクト作成

---
+++ Codable/Codable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Codable
+//
+//  Created by Yamaguchi Kyoya on 2022/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
