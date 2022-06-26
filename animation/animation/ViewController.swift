0a5ae12e46e3c893ecf1f68e914e60c8f17fceda
kyoya <kyoryu32@gmail.com>
2022-06-27 03:56:50 +0900

    サンプルアプリ作成

--- animation/animation/ViewController.swift
+++ animation/animation/ViewController.swift
@@ -1,19 +1,29 @@
-//
-//  ViewController.swift
-//  animation
-//
-//  Created by Yamaguchi Kyoya on 2022/06/27.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var animationView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func goDown() {
+        UIView.animate(withDuration: 1, delay: 0, animations: {
+            self.animationView.center.y += 50
+        }, completion: nil)
+    }
+    
+    @IBAction func rotate() {
+        UIView.animate(withDuration: 1, delay: 0, animations: {
+            self.animationView.transform = CGAffineTransform(rotationAngle: .pi)
+        }, completion: nil)
+    }
+    
+    @IBAction func blink() {
+        UIView.animate(withDuration: 0.5, delay: 0, options: [.autoreverse, .repeat], animations: {
+            self.animationView.alpha = 0
+        }, completion: nil)
+    }
 }
 
