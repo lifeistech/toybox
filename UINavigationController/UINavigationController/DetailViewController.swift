e08499778f8dd27df6c48480a3101af710dffe9c
kyoya <kyoryu32@gmail.com>
2022-06-27 18:17:24 +0900

    add: largeTitleの切り替え

--- UINavigationController/UINavigationController/DetailViewController.swift
+++ UINavigationController/UINavigationController/DetailViewController.swift
@@ -5,5 +5,7 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "詳細"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .always
     }
 }
--- UINavigationController/UINavigationController/TopViewController.swift
+++ UINavigationController/UINavigationController/TopViewController.swift
@@ -5,6 +5,7 @@ class TopViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "トップ"
+        navigationItem.largeTitleDisplayMode = .never
     }
     
     @IBAction func showDetail() {
