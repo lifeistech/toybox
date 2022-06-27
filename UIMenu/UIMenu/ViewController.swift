98850f6012cc1d48595646ef4c0bf3de91d5a6a5
kyoya <kyoryu32@gmail.com>
2022-06-27 19:24:56 +0900

    update: サンプルプロジェクト作成

--- UIMenu/UIMenu/ViewController.swift
+++ UIMenu/UIMenu/ViewController.swift
@@ -1,19 +1,25 @@
-//
-//  ViewController.swift
-//  UIMenu
-//
-//  Created by Yamaguchi Kyoya on 2022/06/27.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var weatherImageView: UIImageView!
+    @IBOutlet var menuButton: UIButton! {
+        didSet {
+            let titles = ["晴れ", "曇り", "雨"]
+            let images = [UIImage(systemName: "sun.max"), UIImage(systemName: "cloud"), UIImage(systemName: "cloud.rain")]
+            var menuItems = [UIAction]()
+            for i in 0..<titles.count {
+                menuItems.append(UIAction(title: titles[i], image: images[i], handler: { _ in
+                    self.weatherImageView.image = images[i]
+                }))
+            }
+            menuButton.menu = UIMenu(title: "画像を選択", image: nil, children: menuItems)
+            menuButton.showsMenuAsPrimaryAction = true
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
