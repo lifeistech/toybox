87295b50dc88a4b93c0f434094c0bdd7a5627b5f
risako070310 <risako070310@gmail.com>
2022-07-01 02:33:11 +0900

    add: UISwitch コメント

--- UISwitch/UISwitch/ViewController.swift
+++ UISwitch/UISwitch/ViewController.swift
@@ -5,6 +5,7 @@ class ViewController: UIViewController {
     @IBOutlet var lightImageView: UIImageView!
     @IBOutlet var lightSwitch: UISwitch! {
         didSet {
+            //最初のスイッチの状態を定義
             lightSwitch.setOn(false, animated: false)
         }
     }
@@ -16,9 +17,11 @@ class ViewController: UIViewController {
     @IBAction func didTapSwitch() {
         //関連付けするactionはValue Changed
         if lightSwitch.isOn {
+            //スイッチの状態が変化したときに画像を更新する(オンの場合)
             lightImageView.image = UIImage(systemName: "lightbulb.fill")
             lightImageView.tintColor = .systemYellow
         } else {
+            //スイッチの状態が変化したときに画像を更新する(オフの場合)
             lightImageView.image = UIImage(systemName: "lightbulb")
             lightImageView.tintColor = .lightGray
         }
