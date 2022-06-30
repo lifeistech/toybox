186031fbafbf56d114e4ee2fab987758cd45416f
kyoya <kyoryu32@gmail.com>
2022-06-30 21:51:41 +0900

    add: 配列をenumに置き換え

--- UIMenu/UIMenu/ViewController.swift
+++ UIMenu/UIMenu/ViewController.swift
@@ -5,13 +5,11 @@ class ViewController: UIViewController {
     @IBOutlet var weatherImageView: UIImageView!
     @IBOutlet var menuButton: UIButton! {
         didSet {
-            let titles = ["晴れ", "曇り", "雨"]
-            let images = [UIImage(systemName: "sun.max"), UIImage(systemName: "cloud"), UIImage(systemName: "cloud.rain")]
             var menuItems = [UIAction]()
-            for i in 0..<titles.count {
-                menuItems.append(UIAction(title: titles[i], image: images[i], handler: { _ in
-                    self.weatherImageView.image = images[i]
-                }))
+            for weather in Weather.allCases {
+                menuItems.append(UIAction(title: weather.title, image: weather.image) { _ in
+                    self.weatherImageView.image = weather.image
+                })
             }
             menuButton.menu = UIMenu(title: "画像を選択", image: nil, children: menuItems)
             menuButton.showsMenuAsPrimaryAction = true
