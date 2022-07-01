5ef1183485ea90a78d74c77b90d92a466707758c
risako070310 <risako070310@gmail.com>
2022-07-01 23:49:47 +0900

    add: UIPickerView コメント

--- UIPickerView/UIPickerView/ViewController.swift
+++ UIPickerView/UIPickerView/ViewController.swift
@@ -5,6 +5,7 @@ class ViewController: UIViewController {
     @IBOutlet var selectionLabel: UILabel!
     @IBOutlet var devicePicker: UIPickerView! {
         didSet {
+            //UIPickerViewのDelegateとDataSourceにUIViewControllerをセット
             devicePicker.delegate = self
             devicePicker.dataSource = self
         }
@@ -18,18 +19,22 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    //UIPickerViewの列の数
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     
+    //UIPickerViewの選択肢の数
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return deviceList.count
     }
     
+    //UIPickerViewの要素をセット
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return deviceList[row]
     }
     
+    //UIPickerViewの要素が選択されたときの処理
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         selectionLabel.text = deviceList[row]
     }
