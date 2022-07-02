e01b32000ee99bebfe4a6333e24cee4096e3b185
risako070310 <risako070310@gmail.com>
2022-07-02 23:59:20 +0900

    add: UIMenuのコメントを追加

--- UIMenu/UIMenu/ViewController.swift
+++ UIMenu/UIMenu/ViewController.swift
@@ -5,7 +5,9 @@ class ViewController: UIViewController {
     @IBOutlet var weatherImageView: UIImageView!
     @IBOutlet var menuButton: UIButton! {
         didSet {
+            //メニューの要素を追加
             var menuItems = [UIAction]()
+            //選択によって画像を変更
             for weather in Weather.allCases {
                 menuItems.append(UIAction(title: weather.title, image: weather.image) { _ in
                     self.weatherImageView.image = weather.image
