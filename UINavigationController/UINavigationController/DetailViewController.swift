063120af2ae4d8d832c578e8c6a3a64486663b79
risako070310 <risako070310@gmail.com>
2022-07-02 18:48:23 +0900

    add: UINavigationController コメントの追加

--- UINavigationController/UINavigationController/DetailViewController.swift
+++ UINavigationController/UINavigationController/DetailViewController.swift
@@ -5,6 +5,7 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "詳細"
+        //NavigationBarのタイトル表示のカスタマイズ
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.largeTitleDisplayMode = .always
     }
--- UINavigationController/UINavigationController/TopViewController.swift
+++ UINavigationController/UINavigationController/TopViewController.swift
@@ -5,9 +5,11 @@ class TopViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "トップ"
+        //NavigationBarのタイトル表示を常に小さくする設定
         navigationItem.largeTitleDisplayMode = .never
     }
     
+    //ボタンが押されたときにSegueを実行
     @IBAction func showDetail() {
         performSegue(withIdentifier: "toDetail", sender: nil)
     }
