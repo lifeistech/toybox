91b01da2d060b711cd055130436fe5ee02456d9c
risako070310 <risako070310@gmail.com>
2022-07-07 16:26:50 +0900

    add: Codableのコメントを追加

--- Codable/Codable/ViewController.swift
+++ Codable/Codable/ViewController.swift
@@ -5,14 +5,17 @@ class ViewController: UIViewController {
     @IBOutlet var titleTextField: UITextField!
     @IBOutlet var contentTextView: UITextView!
     
+   
     override func viewDidLoad() {
         super.viewDidLoad()
+        //UserDefaultsに入ってるメモデータを変数に代入。UserDefaultsの中のデータがOptionalなので、if letでnil判定をしてから表示を実行します
         if let savedData = UserDefaults.standard.data(forKey: "memo"), let decoded = try? JSONDecoder().decode(MemoData.self, from: savedData) {
             titleTextField.text = decoded.title
             contentTextView.text = decoded.content
         }
     }
     
+    //TextFieldsに入力した値をUserDefaultsに保存する
     @IBAction func saveMemo() {
         let memoData = MemoData(title: titleTextField.text ?? "", content: contentTextView.text ?? "")
         if let encoded = try? JSONEncoder().encode(memoData) {
