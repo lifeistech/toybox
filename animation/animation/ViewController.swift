f8514eaaab6b5564c61f9bfeaf54b8b6f86a7ae5
risako070310 <risako070310@gmail.com>
2022-07-08 18:10:13 +0900

    update: コードのコメントを更新。ラジアンの説明を追加

--- animation/animation/ViewController.swift
+++ animation/animation/ViewController.swift
@@ -16,14 +16,14 @@ class ViewController: UIViewController {
     }
     
     @IBAction func goUpWithEaseOut() {
-        //y軸の座標を100上に動かすアニメーション
+        //y軸の座標を100上に動かすアニメーション。イーズアウトを適応しています。
         UIView.animate(withDuration: 1, delay: 0, options: .curveEaseOut) {
             self.animationView.center.y -= 100
         }
     }
     
     @IBAction func rotate() {
-        //回転するアニメーション
+        //回転するアニメーション。回転角の単位はラジアンなので、この場合はπ=180°(その他の例: π/2=90°, 2π=360°)
         UIView.animate(withDuration: 1, delay: 0) {
             self.animationView.transform = CGAffineTransform(rotationAngle: .pi)
         }
