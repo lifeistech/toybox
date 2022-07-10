e7d30f753f40018b78995d6adff4ef3aef145fd4
Masakaz Ozaki <masakaz@ieee.org>
2022-07-11 04:01:42 +0900

    feat: add blur and vibrancy effect style

--- UIVisualEffectView/UIVisualEffectView/ViewController.swift
+++ UIVisualEffectView/UIVisualEffectView/ViewController.swift
@@ -1,19 +1,26 @@
-//
-//  ViewController.swift
-//  UIVisualEffectView
-//
-//  Created by Masakaz Ozaki on 2022/07/11.
-//
-
 import UIKit
-
+//TODO: コメントは適当なのでREADME フェーズできちんと書く
 class ViewController: UIViewController {
+
+    //Blur View
+    @IBOutlet var blurView: UIVisualEffectView! {
+        didSet {
+            //さまざまなエフェクトを指定可能(Storybaordでも指定可能)
+            blurView.effect = UIBlurEffect(style: .regular)
+        }
+    }
+
+    //VibrancyEffectView
+    //TODO: READMEかここかどちらかにVibrancyとBlurの違いを説明する
+    @IBOutlet var vibrancyView: UIVisualEffectView! {
+        didSet {
+            //さまざまなスタイルのVibrancyを定義可能(storyboardでも指定可能)
+            vibrancyView.effect = UIVibrancyEffect(blurEffect: UIBlurEffect(style: .regular), style: .label)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
