46fcdfe8796cb718d89828b40cca8788654af4a4
risako070310 <risako070310@gmail.com>
2022-07-15 05:51:48 +0900

    create: プロジェクト作成

---
+++ if/if/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  if
+//
+//  Created by Risako Kato on 2022/07/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
