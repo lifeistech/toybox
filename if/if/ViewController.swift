4775b706720e04b0efcc10ee3f1fbef415467057
risako070310 <risako070310@gmail.com>
2022-07-15 06:04:25 +0900

    add: UISwitchを使用したサンプルを追加

--- if/if/ViewController.swift
+++ if/if/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var switchLabel: UILabel!
+    
+    @IBOutlet var uiSwitch: UISwitch! {
+        didSet {
+            uiSwitch.setOn(false, animated: false)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func didTapSwitch() {
+        //関連付けするactionはValue Changed
+        if uiSwitch.isOn {
+            switchLabel.text = "スイッチがオンです"
+        } else {
+            switchLabel.text = "スイッチがオフです"
+        }
+    }
 
 }
 
