3e1042ffd42c92f7a2f609a88b0219cfe10b9dfc
risako070310 <risako070310@gmail.com>
2022-07-15 06:16:41 +0900

    add: 複数条件のif文サンプルを追加

--- if/if/ViewController.swift
+++ if/if/ViewController.swift
@@ -16,10 +16,23 @@ class ViewController: UIViewController {
             uiSwitch.setOn(false, animated: false)
         }
     }
+    
+    @IBOutlet var resultLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let num1: Int = 8
+        let num2: Int = 24
+        
+        if num1 >= 10 && num2 >= 10 {
+            resultLabel.text = "num1とnum2はどちらも10以上です"
+        } else if num1 >= 10 || num2 >= 10 {
+            resultLabel.text = "num1かnum2のどちらか一方が10以上です"
+        } else {
+            resultLabel.text = "num1とnum2はどちらも10未満です"
+        }
+        
     }
 
     @IBAction func didTapSwitch() {
