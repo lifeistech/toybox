c5821d6ef8922875e501d2516923b8154df98f84
risako070310 <risako070310@gmail.com>
2022-07-16 16:09:53 +0900

    add: コードへコメントを追加

--- Speech/Speech/ViewController.swift
+++ Speech/Speech/ViewController.swift
@@ -3,6 +3,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // peechSynthesizerクラスのインスタンスを宣言
     let speechSynthesizer = AVSpeechSynthesizer()
     
     override func viewDidLoad() {
@@ -10,8 +11,11 @@ class ViewController: UIViewController {
     }
     
     @IBAction func speech() {
+        // 読み上げの内容をセット
         let utterance = AVSpeechUtterance(string: "ライフイズテックは、中学生・高校生向けIT・プログラミング教育サービスです。")
+        // 言語を日本に設定
         utterance.voice = AVSpeechSynthesisVoice(language: "ja-JP")
+        // 読み上げの実行
         speechSynthesizer.speak(utterance)
     }
 }
