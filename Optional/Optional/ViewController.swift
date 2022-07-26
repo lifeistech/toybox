0add01e7e14a3259d7ca834e025a1deacc0c1062
kyoya <kyoryu32@gmail.com>
2022-07-26 23:25:08 +0900

    update: サンプルアプリ実装

--- Optional/Optional/ViewController.swift
+++ Optional/Optional/ViewController.swift
@@ -1,19 +1,45 @@
-//
-//  ViewController.swift
-//  Optional
-//
-//  Created by Yamaguchi Kyoya on 2022/07/26.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var resultLabel: UILabel!
+    
+    var number: Int?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func forceUnwrap() {
+        resultLabel.text = String(number!)
+    }
+    
+    @IBAction func unwrapWithIfLet() {
+        if let number = number {
+            resultLabel.text = String(number)
+        } else {
+            resultLabel.text = "numberはnilです"
+        }
+    }
+    
+    @IBAction func unwrapWithGuardLet() {
+        guard let number = number else {
+            resultLabel.text = "numberはnilです"
+            return
+        }
+        resultLabel.text = String(number)
+    }
+    
+    @IBAction func defaultValue() {
+        resultLabel.text = String(number ?? 0)
+    }
+    
+    @IBAction func switchNilAndSome(sender: UISwitch) {
+        if sender.isOn {
+            number = 10
+        } else {
+            number = nil
+        }
+    }
 }
 
