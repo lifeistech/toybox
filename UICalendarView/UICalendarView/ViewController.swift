b7befd0f0cbec663c22e865dd654e6d574744e8c
kyoya <kyoryu32@gmail.com>
2022-07-27 00:21:25 +0900

    update: サンプルアプリ作成

--- UICalendarView/UICalendarView/ViewController.swift
+++ UICalendarView/UICalendarView/ViewController.swift
@@ -1,19 +1,49 @@
-//
-//  ViewController.swift
-//  UICalendarView
-//
-//  Created by Yamaguchi Kyoya on 2022/07/26.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var calendarView: UICalendarView {
+        let calendarView = UICalendarView(frame: view.frame)
+        let gregorianCalendar = Calendar(identifier: .gregorian)
+        calendarView.calendar = gregorianCalendar
+        calendarView.locale = Locale(identifier: "ja_JP")
+        calendarView.fontDesign = .rounded
+        calendarView.delegate = self
+        calendarView.selectionBehavior = UICalendarSelectionSingleDate(delegate: self)
+        return calendarView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(calendarView)
     }
-
-
 }
 
+extension ViewController: UICalendarViewDelegate {
+    func calendarView(_ calendarView: UICalendarView, decorationFor dateComponents: DateComponents) -> UICalendarView.Decoration? {
+        switch dateComponents.day {
+        case 1:
+            return .default()
+        case 2:
+            return .default(color: .systemGreen)
+        case 3:
+            return .image(UIImage(systemName: "pencil"), color: .systemTeal, size: .large)
+        case 10:
+            return .customView {
+                let label = UILabel()
+                label.text = "School"
+                return label
+            }
+        default:
+            return nil
+        }
+    }
+}
+
+extension ViewController: UICalendarSelectionSingleDateDelegate {
+    func dateSelection(_ selection: UICalendarSelectionSingleDate, didSelectDate dateComponents: DateComponents?) {
+        if let month = dateComponents?.month, let day = dateComponents?.day {
+            print("\(month)月\(day)日")
+        }
+    }
+}
