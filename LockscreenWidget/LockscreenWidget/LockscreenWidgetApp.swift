5832d6dbb0a41653ce4ec2d295e5700ef270259a
Masakaz Ozaki <masakaz@ieee.org>
2022-07-27 17:57:20 +0900

    feat: create proj

---
+++ LockscreenWidget/LockscreenWidget/LockscreenWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockscreenWidgetApp.swift
+//  LockscreenWidget
+//
+//  Created by Masakaz Ozaki on 2022/07/27.
+//
+
+import SwiftUI
+
+@main
+struct LockscreenWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
