d24477126ff5e29d9036683e496e6136f30e9231
Masakaz Ozaki <masakaz@ieee.org>
2022-07-27 17:41:10 +0900

    impl

---
+++ SwiftCharts/SwiftCharts/ToyShape.swift
@@ -0,0 +1,15 @@
+//
+//  ChartContent.swift
+//  SwiftCharts
+//
+//  Created by Masakaz Ozaki on 2022/07/26.
+//
+
+import Foundation
+
+struct ToyShape: Identifiable {
+    var color: String
+    let type: String
+    let count: Int
+    let id = UUID()
+}
