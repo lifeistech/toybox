92b107f5192a6c9f2db99282095d1eb37ba55e87
kyoya <kyoryu32@gmail.com>
2022-08-03 20:29:44 +0900

    feat: サンプルアプリ作成

---
+++ UIImagePicker/README.md
@@ -0,0 +1,5 @@
+#  <#Title#>
+
+実装方法(readmeを書く時にわかりやすいように書き換える)
+
+Info.plist に Privacy - Camera Usage Description と Privacy - Photo Library Usage Descriptionを追加
--- UIImagePicker/UIImagePicker/ViewController.swift
+++ UIImagePicker/UIImagePicker/ViewController.swift
@@ -1,19 +1,31 @@
-//
-//  ViewController.swift
-//  UIImagePicker
-//
-//  Created by Yamaguchi Kyoya on 2022/08/03.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    var imagePicker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
     }
 
-
+    @IBAction func openCamera() {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true)
+    }
+    
+    @IBAction func openPhotoLibrary() {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true)
+    }
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            imageView.image = image
+        }
+        dismiss(animated: true)
+    }
+}
