d52f929cba735d82d5ae0a9b852a9c38ccf022c4
risako070310 <risako070310@gmail.com>
2022-08-03 17:04:02 +0900

    add: viewcontroller.swiftでUIMenuを実装

--- enum/enum/ViewController.swift
+++ enum/enum/ViewController.swift
@@ -1,19 +1,25 @@
-//
-//  ViewController.swift
-//  enum
-//
-//  Created by Risako Kato on 2022/08/03.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var hourImageView: UIImageView!
+    @IBOutlet var menuButton: UIButton! {
+        didSet {
+            //メニューの要素を追加
+            var menuItems = [UIAction]()
+            //選択によって画像を変更
+            for hour in Hours.allCases {
+                menuItems.append(UIAction(title: hour.title, image: hour.image) { _ in
+                    self.hourImageView.image = hour.image
+                    self.hourImageView.tintColor = .systemPink
+                })
+            }
+            menuButton.menu = UIMenu(title: "画像を選択", image: nil, children: menuItems)
+            menuButton.showsMenuAsPrimaryAction = true
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
