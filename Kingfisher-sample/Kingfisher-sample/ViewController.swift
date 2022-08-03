e98c0e58b25f34490c50b0d30073de75e827b5d6
kyoya <kyoryu32@gmail.com>
2022-08-03 21:13:46 +0900

    feat: サンプルアプリ作成

--- Kingfisher-sample/Kingfisher-sample/ViewController.swift
+++ Kingfisher-sample/Kingfisher-sample/ViewController.swift
@@ -1,19 +1,16 @@
-//
-//  ViewController.swift
-//  Kingfisher-sample
-//
-//  Created by Yamaguchi Kyoya on 2022/08/03.
-//
-
 import UIKit
+import Kingfisher
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView! {
+        didSet {
+            imageView.kf.setImage(with: URL(string: "https://life-is-tech.com/index/img/img-sitenav-school2020-pc.jpg")!)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
