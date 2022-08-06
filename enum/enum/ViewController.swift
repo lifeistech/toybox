85927a90224880b6a1ab21d19766827e734b5cba
risako070310 <risako070310@gmail.com>
2022-08-06 12:05:58 +0900

    update: [weak self]でキャプチャ

--- enum/enum/ViewController.swift
+++ enum/enum/ViewController.swift
@@ -9,9 +9,9 @@ class ViewController: UIViewController {
             var menuItems = [UIAction]()
             //選択によって画像を変更
             for hour in Hours.allCases {
-                menuItems.append(UIAction(title: hour.title, image: hour.image) { _ in
-                    self.hourImageView.image = hour.image
-                    self.hourImageView.tintColor = .systemPink
+                menuItems.append(UIAction(title: hour.title, image: hour.image) {[weak self] _ in
+                    self?.hourImageView.image = hour.image
+                    self?.hourImageView.tintColor = .systemPink
                 })
             }
             menuButton.menu = UIMenu(title: "画像を選択", image: nil, children: menuItems)
