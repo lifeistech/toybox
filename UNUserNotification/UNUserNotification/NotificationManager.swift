a20a203cabce2be7abe3c5d0de6d5dcb5dcaa6b0
risako070310 <risako070310@gmail.com>
2022-08-10 08:28:49 +0900

    add: UNUserNotificationのコードにコメントを追加

--- UNUserNotification/UNUserNotification/NotificationManager.swift
+++ UNUserNotification/UNUserNotification/NotificationManager.swift
@@ -4,40 +4,50 @@ import NotificationCenter
 
 class NotificationManager {
     
+    //アラートウィジェットで選択した通知のリクエストを登録
     static func setTimeIntervalNotification(title: String, timeInterval: TimeInterval) {
         UserNotifications.UNUserNotificationCenter.current().requestAuthorization(options:[.badge, .alert, .sound]) { granted, error in
             if granted {
+                //triggerとは通知が送られる条件(今回の場合は時間経過)
                 let trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInterval, repeats: false)
+                //contentとは実際に通知に表示される内容
                 let notificationContent = UNMutableNotificationContent()
                 notificationContent.title = title
+                //contentとtriggerを持つリクエストを作成
                 let request = UNNotificationRequest(identifier: UUID().uuidString, content: notificationContent, trigger: trigger)
                 UNUserNotificationCenter.current().add(request)
             }
         }
     }
     
+    //アラートウィジェットで設定した日時の通知のリクエストを登録
     static func setCalendarNotification(title: String, date: Date) {
         UserNotifications.UNUserNotificationCenter.current().requestAuthorization(options:[.badge, .alert, .sound]) { granted, error in
             if granted {
                 let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: date)
+                //この場合はtriggerに上記のdateComponents(指定日時)を設定している
                 let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
                 let notificationContent = UNMutableNotificationContent()
                 notificationContent.title = title
+                //contentとtriggerを持つリクエストを作成
                 let request = UNNotificationRequest(identifier: UUID().uuidString, content: notificationContent, trigger: trigger)
                 UNUserNotificationCenter.current().add(request)
             }
         }
     }
     
+    //アラートウィジェットで設定した場所の通知のリクエストを登録
     static func setLocationNotification(title: String, coordinate: CLLocationCoordinate2D) {
         UserNotifications.UNUserNotificationCenter.current().requestAuthorization(options:[.badge, .alert, .sound]) { granted, error in
             if granted {
                 let region = CLCircularRegion(center: coordinate, radius: 500.0, identifier: UUID().uuidString)
                 region.notifyOnEntry = true
                 region.notifyOnExit = false
+                //この場合はtriggerに上記のregion(場所)を指定している
                 let trigger = UNLocationNotificationTrigger(region: region, repeats: false)
                 let notificationContent = UNMutableNotificationContent()
                 notificationContent.title = title
+                //contentとtriggerを持つリクエストを作成
                 let request = UNNotificationRequest(identifier: UUID().uuidString, content: notificationContent, trigger: trigger)
                 UNUserNotificationCenter.current().add(request)
             }
--- UNUserNotification/UNUserNotification/ViewController.swift
+++ UNUserNotification/UNUserNotification/ViewController.swift
@@ -3,27 +3,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //指定日時をセットするDatePicker
     @IBOutlet var notificationDatePicker: UIDatePicker!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    //ボタンをタップしたときに表示されるアラートウィジェットを実装
     @IBAction func setTimeIntervalNotification() {
         let intervals = [10, 30, 60]
         let alert = UIAlertController(title: "時間を指定", message: nil, preferredStyle: .alert)
+        //10,30,60秒それぞれにタップしたら指定時間経過後の通知を選択
         for interval in intervals {
             alert.addAction(UIAlertAction(title: "\(interval)秒", style: .default) {
                 _ in
                 NotificationManager.setTimeIntervalNotification(title: "\(interval)秒経ちました", timeInterval: TimeInterval(interval))
             })
         }
+        //キャンセルボタン
         alert.addAction(UIAlertAction(title: "キャンセル", style: .cancel))
         present(alert, animated: true)
     }
     
+    //ボタンをタップしたときに表示されるアラートウィジェットを実装
     @IBAction func setCalenderNotification() {
         let alert = UIAlertController(title: "指定日時に通知を設定しますか？", message: nil, preferredStyle: .alert)
+        //DatePickerで指定した日時の通知を選択
         alert.addAction(UIAlertAction(title: "はい", style: .default) {
             _ in
             NotificationManager.setCalendarNotification(title: "指定日時になりました", date: self.notificationDatePicker.date)
@@ -32,12 +38,14 @@ class ViewController: UIViewController {
         present(alert, animated: true)
     }
     
+    //ボタンをタップしたときに表示されるアラートウィジェットを実装
     @IBAction func setLocationNotification() {
         let locations = [
             ("Life is Tech! 東京オフィス", CLLocationCoordinate2D(latitude: 35.6469, longitude: 139.735869)),
             ("Life is Tech! 大阪オフィス", CLLocationCoordinate2D(latitude: 34.70493918, longitude: 135.50383534))
         ]
         let alert = UIAlertController(title: "場所を指定", message: nil, preferredStyle: .alert)
+        //Life is Tech!東京・大阪オフィスそれぞれタップした場所での通知を選択
         for location in locations {
             alert.addAction(UIAlertAction(title: location.0, style: .default) {
                 _ in
