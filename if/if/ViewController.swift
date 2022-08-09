e0d876618d81f264b741e5559b06a163784490be
risako070310 <risako070310@gmail.com>
2022-08-10 07:08:49 +0900

    add: ifのコードにコメントを追加

--- if/if/ViewController.swift
+++ if/if/ViewController.swift
@@ -29,7 +29,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func switchValueChanged() {
-        //関連付けするactionはValue Changed
+        //スイッチのオンオフによって条件分岐するif文(UISwitch.isOnはBool型なので==trueを省略できる)
         if uiSwitch.isOn {
             switchStatusLabel.text = "スイッチがオンです"
         } else {
@@ -37,22 +37,28 @@ class ViewController: UIViewController {
         }
     }
     
+    //num1のカウントボタン
     @IBAction func num1PlusButtonTapped(){
         num1 += 1
         checkNumberValue()
     }
     
+    //num2のカウントボタン
     @IBAction func num2PlusButtonTapped(){
         num2 += 1
         checkNumberValue()
     }
     
     func checkNumberValue(){
+        //num1とnum2の値が10以上であるか10未満であるかで条件分岐するif文（&&を使って2つの条件文を結んでいる)
         if num1 >= 10 && num2 >= 10 {
+            //num1とnum2が両方10以上だった場合
             resultLabel.text = "num1とnum2はどちらも10以上です"
         } else if num1 >= 10 || num2 >= 10 {
+            //num1とnum2の片方が10以上だった場合
             resultLabel.text = "num1かnum2のどちらか一方が10以上です"
         } else {
+            //num1とnum2が両方10未満だった場合
             resultLabel.text = "num1とnum2はどちらも10未満です"
         }
     }
