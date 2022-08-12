62a742cad54bf09d9dc544a30c7703e43857ad61
igaryo0506 <igarashi-ryo0506@g.ecc.u-tokyo.ac.jp>
2022-08-12 21:21:30 +0900

    add: MapViewの実装

--- MKMapView/MKMapView/ViewController.swift
+++ MKMapView/MKMapView/ViewController.swift
@@ -6,12 +6,19 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: CLLocationCoordinate2DMake(35.6466594,139.7351873), span:span)
+        mapView.region = region
     }
 
 
