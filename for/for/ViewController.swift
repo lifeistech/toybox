c70905a452c6faab9e80e1e0cbb0e47c719e4093
risako070310 <risako070310@gmail.com>
2022-08-17 18:54:49 +0900

    add: forのコードへコメントを追加

--- for/for/ViewController.swift
+++ for/for/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet var menuButton: UIButton! {
         didSet {
             var menuItems = [UIAction]()
+            //UIMenuに要素を追加 配列の数だけ繰り返すfor文
             for transportation in Transportation.allCases {
                 menuItems.append(UIAction(title: transportation.rawValue) {[weak self] _ in
                     self?.transportationLabel.text = "\(transportation.rawValue)が選択されました"
@@ -26,12 +27,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //繰り返しの回数を10回繰り返しprintするfor-in文
         for i in 1...10 {
             print("print -> \(i)")
         }
         
-        let numbers = ["a", "b", "c", "d", "e"]
-        numbers.forEach {
+        //alphabets配列の中身をprintするforEach文
+        let alphabets = ["a", "b", "c", "d", "e"]
+        alphabets.forEach {
             print("print -> \($0)")
         }
     }
