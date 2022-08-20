b420ade6af59b4e5672bca572ebfcb04a145c7a3
igaryo0506 <igarashi-ryo0506@g.ecc.u-tokyo.ac.jp>
2022-08-20 14:11:12 +0900

    add : 現在地ボタンの追加

--- MKMapView/MKMapView/ViewController.swift
+++ MKMapView/MKMapView/ViewController.swift
@@ -7,14 +7,19 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
     
     @IBOutlet var mapView: MKMapView!
-
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
         
         // 地図の表示範囲を指定
         let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
@@ -33,5 +38,36 @@ class ViewController: UIViewController {
         pin.coordinate = CLLocationCoordinate2D(latitude: 35.6466, longitude: 139.7351)
         // MapViewにpinを追加
         mapView.addAnnotation(pin)
+        
+    }
+    
+    // ボタンが押された時に現在地を取得する
+    @IBAction func currentLocation(){
+        locationManager!.requestWhenInUseAuthorization()
+        // 位置情報を更新
+        mapView.region.center = mapView.userLocation.coordinate
+    }
+}
+
+// CLLocationManagerに関するextension
+extension ViewController: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+            // 許可されてない場合
+        case .notDetermined:
+            // 許可を求める
+            manager.requestWhenInUseAuthorization()
+            // 拒否されてる場合
+        case .restricted, .denied:
+            // 何もしない
+            break
+            // 許可されている場合
+        case .authorizedAlways, .authorizedWhenInUse:
+            // 現在地の取得を開始
+            manager.startUpdatingLocation()
+            break
+        default:
+            break
+        }
     }
 }
