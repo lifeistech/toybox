f0eb13d19f413c1b840542113b354ff2f41d600e
risako070310 <risako070310@gmail.com>
2022-08-22 21:36:35 +0900

    add: UICalendarViewのコードにコメントを追加

--- UICalendarView/UICalendarView/ViewController.swift
+++ UICalendarView/UICalendarView/ViewController.swift
@@ -1,33 +1,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    //UICalendarViewを定義
     var calendarView: UICalendarView {
         let calendarView = UICalendarView(frame: view.frame)
         let gregorianCalendar = Calendar(identifier: .gregorian)
         calendarView.calendar = gregorianCalendar
         calendarView.locale = Locale(identifier: "ja_JP")
+        //カレンダーのフォントを変更
         calendarView.fontDesign = .rounded
+        //UICalendarViewのdelegateにUIViewControllerをセット
         calendarView.delegate = self
+        //選択できる日数を1日に設定
         calendarView.selectionBehavior = UICalendarSelectionSingleDate(delegate: self)
         return calendarView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //定義したカレンダービューを表示
         view.addSubview(calendarView)
     }
 }
 
+//日付ごとのデコレーションを設定
 extension ViewController: UICalendarViewDelegate {
     func calendarView(_ calendarView: UICalendarView, decorationFor dateComponents: DateComponents) -> UICalendarView.Decoration? {
         switch dateComponents.day {
+        //日付の下のアクセント(デフォルトカラーはグレー)
         case 1:
             return .default()
+        //日付の下のアクセント(緑)
         case 2:
             return .default(color: .systemGreen)
+        //SFSymbolを表示
         case 3:
             return .image(UIImage(systemName: "pencil"), color: .systemTeal, size: .large)
+        //UILabelを表示
         case 10:
             return .customView {
                 let label = UILabel()
@@ -40,6 +49,7 @@ extension ViewController: UICalendarViewDelegate {
     }
 }
 
+//カレンダーのタップした日付をprintする
 extension ViewController: UICalendarSelectionSingleDateDelegate {
     func dateSelection(_ selection: UICalendarSelectionSingleDate, didSelectDate dateComponents: DateComponents?) {
         if let month = dateComponents?.month, let day = dateComponents?.day {
