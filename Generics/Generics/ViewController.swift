6cf61f05598abd1a8545bdd4a98eaf8198767294
yukiny0811 <yukiny0811@gmail.com>
2022-08-30 16:03:25 +0900

    feat: Genericsプロジェクト作成

---
+++ Generics/Generics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Generics
+//
+//  Created by クワシマ・ユウキ on 2022/08/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
