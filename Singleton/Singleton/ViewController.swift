7d25b0f09334b538be156a6525d6c4258062c75c
AtsushiHosaka <hosaka2917@gmail.com>
2024-02-14 21:03:43 +0900

    create project

---
+++ Singleton/Singleton/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Singleton
+//
+//  Created by 保坂篤志 on 2024/02/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
